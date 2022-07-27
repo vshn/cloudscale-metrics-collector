ad61dda2debf9c32d35cdc601eb6c010cef85fac
David Gubler <david.gubler@vshn.ch>
2022-07-27 11:39:40 +0200

    Don't use os.Exit() as error handling, it breaks defer

--- main.go
+++ main.go
@@ -150,15 +150,17 @@ func initDb(ctx context.Context, tx *sqlx.Tx) error {
 	return nil
 }
 
-func checkErrExit(err error) {
+func main() {
+	ctx := context.Background()
+	err := sync(ctx)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
 		os.Exit(1)
 	}
+	os.Exit(0)
 }
 
-func main() {
-	ctx := context.Background()
+func sync(ctx context.Context) error {
 	cloudscaleApiToken, dbUrl := cfg()
 
 	cloudscaleClient := cloudscale.NewClient(http.DefaultClient)
@@ -166,28 +168,42 @@ func main() {
 
 	// The cloudscale API works in Europe/Zurich, so we have to use the same, regardless of where this code runs
 	location, err := time.LoadLocation("Europe/Zurich")
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 
 	// Fetch statistics of yesterday (as per Europe/Zurich). The metrics will cover the entire day.
 	now := time.Now().In(location)
 	date := time.Date(now.Year(), now.Month(), now.Day()-1, 0, 0, 0, 0, now.Location())
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 
 	rdb, err := db.Openx(dbUrl)
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 	defer rdb.Close()
 
 	// initialize DB
 	tx, err := rdb.BeginTxx(ctx, &sql.TxOptions{})
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 	defer tx.Rollback()
 	err = initDb(ctx, tx)
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 	err = tx.Commit()
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 
 	accumulated, err := accumulate(ctx, date, cloudscaleClient)
-	checkErrExit(err)
+	if err != nil {
+		return err
+	}
 
 	for source, value := range accumulated {
 		if value == 0 {
@@ -198,26 +214,40 @@ func main() {
 
 		// start new transaction for actual work
 		tx, err = rdb.BeginTxx(ctx, &sql.TxOptions{})
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		tenant, err := tenantsmodel.Ensure(ctx, tx, &db.Tenant{Source: source.Tenant})
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		category, err := categoriesmodel.Ensure(ctx, tx, &db.Category{Source: source.Zone + ":" + source.Namespace})
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		dateTime := datetimesmodel.New(source.Start)
 		dateTime, err = datetimesmodel.Ensure(ctx, tx, dateTime)
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		product, err := productsmodel.GetBestMatch(ctx, tx, source.String(), source.Start)
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		discount, err := discountsmodel.GetBestMatch(ctx, tx, source.String(), source.Start)
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		query, err := queriesmodel.GetByName(ctx, tx, source.Query+":"+source.Zone)
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		var quantity float64
 		if query.Unit == "GB" || query.Unit == "GBDay" {
@@ -225,15 +255,19 @@ func main() {
 		} else if query.Unit == "KReq" {
 			quantity = float64(value) / 1000
 		} else {
-			checkErrExit(errors.New("Unknown query unit " + query.Unit))
+			return errors.New("Unknown query unit " + query.Unit)
 		}
 		storageFact := factsmodel.New(dateTime, query, tenant, category, product, discount, quantity)
 		_, err = factsmodel.Ensure(ctx, tx, storageFact)
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 
 		err = tx.Commit()
-		checkErrExit(err)
+		if err != nil {
+			return err
+		}
 	}
 
-	os.Exit(0)
+	return nil
 }
